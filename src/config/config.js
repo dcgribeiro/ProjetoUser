098623a3023b08b36259261dd3124b200de64c9b
dcgribeiro <dcgribeiro@hotmail.com>
2020-10-09 20:55:21 -0300

    Alteracao das conexoes

--- src/config/config.js
+++ src/config/config.js
@@ -2,12 +2,12 @@
 module.exports = {
     development: {
         database: {
-            host: 'ls-1e902954d5588c29189bea3a35415ab318d56d1a.ceefo25bexkd.us-east-1.rds.amazonaws.com',
+            host: 'ls-86ba26075ee724b57af4d349fb33b1d3219d0c00.cgciqlwq1xrc.us-east-1.rds.amazonaws.com',
             port: 3306,
             name: 'db_usuario',
             dialect: 'mysql',
             user: 'dbmasteruser',
-            password: 'g.UhB{l_).8n.e&7ZyAXWPT+`:6p|OWT'
+            password: '}4HsQDb|JTy_y}.moT$[Yy,C8.p;}Nr^'
         }
     },
     production: {
